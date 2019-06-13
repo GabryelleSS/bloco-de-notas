c8ba91d89504896e29af0dc2a9af299f475a961d
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-06-13 17:16:37 -0300

    Adicionando funcao de remover post-it

--- js/ceep.js
+++ js/ceep.js
@@ -1,18 +1,71 @@
 const ceep = (function() {
+    const containerNotes = document.querySelector('.container-notes');
+    const elTextNotePad = document.querySelector('textarea');
+    
+    function DrawNotepad() {
+        const btnSave = document.querySelector('.btn-save');
 
-    function _setUpNotepad() {
-        const containerNotes = document.querySelector('.container-notes');
+        btnSave.addEventListener('click', function() {
+            const elDivPostIt = document.createElement('div');
+            elDivPostIt.classList.add('post-it');
+            containerNotes.appendChild(elDivPostIt);
 
-        const confitPostIt = document.querySelectorAll('.btn-config-post-it');
+            const elDivBtnConfig = document.createElement('div');
+            elDivBtnConfig.classList.add('btn-config-post-it');
+            elDivPostIt.appendChild(elDivBtnConfig);
 
-        containerNotes.addEventListener('click', function() {
-            
-            confitPostIt.classList.add('--is-visible');
+            const elBtnTrash = document.createElement('button');
+            elBtnTrash.classList.add('trash');
+
+            const iconTrash = document.createElement('i');
+            iconTrash.classList.add('far');
+            iconTrash.classList.add('fa-trash-alt');
+            elBtnTrash.appendChild(iconTrash);
+
+            elDivBtnConfig.appendChild(elBtnTrash);
+
+            const elBtnRed = document.createElement('button');
+            elBtnRed.classList.add('red');
+            elDivBtnConfig.appendChild(elBtnRed);
+
+            const elBtnGreen= document.createElement('button');
+            elBtnGreen.classList.add('green');
+            elDivBtnConfig.appendChild(elBtnGreen);
+
+            const elBtnBlue = document.createElement('button');
+            elBtnBlue.classList.add('blue');
+            elDivBtnConfig.appendChild(elBtnBlue);
+
+            const elBtnYellow = document.createElement('button');
+            elBtnYellow.classList.add('yellow');
+            elDivBtnConfig.appendChild(elBtnYellow);
+
+            const textContentNotePad = elTextNotePad.value;
+            const elNotePad = document.createElement('p');
+            elNotePad.classList.add('notes');
+            elNotePad.textContent = textContentNotePad;
+            elDivPostIt.appendChild(elNotePad);
+
+            function _btnConfigOn() {
+                elDivPostIt.addEventListener('click', function() {
+                    elDivBtnConfig.classList.toggle('--is-visible');
+                })
+            }
+
+            function _removeContainerNotes() {
+                elBtnTrash.addEventListener('click', function() {
+                    elDivPostIt.remove('post-it');
+                })
+            }
+
+            _btnConfigOn();
+            _removeContainerNotes();
         })
     }
-    
+
     function init() {
-        _setUpNotepad()
+        DrawNotepad();
+        
     }
 
     return {
